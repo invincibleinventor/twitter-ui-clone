9b1bf5583286365c82b20ed42949c218c68dd087
invincibleinventor <invincibleinventor@gmail.com>
2022-11-27 22:18:33 +0530

    completed sidebar

--- src/components/headeritem/headeritem.tsx
+++ src/components/headeritem/headeritem.tsx
@@ -1,5 +1,5 @@
 import { component$, useStore } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { Link, useLocation } from "@builder.io/qwik-city";
 
 interface pros {
     url?: any,
@@ -21,11 +21,12 @@ export default component$((props:pros)=>{
     if(loc.pathname==props.link){
         a.opened=true
     }
+    const link = props.link?.replace('/','')
 
     return(
-        <div class="flex flex-row rounded-full px-[14px] pr-8 w-max py-[10px] space-x-[16px] rounded-full transition-all ease-linear duration-100 cursor-pointer hover:bg-gray-200 items-center content-center">
+        <Link href={link} class="flex flex-row rounded-full px-[14px] pr-8 w-max py-[10px] space-x-[16px] rounded-full transition-all ease-linear duration-100 cursor-pointer hover:bg-gray-200 items-center content-center">
   <img src={props.url}  class="w-8 p-[2px] h-8"></img>
     <span class={`text-black text-xl font-sans ${a.opened?'font-semibold':'font-normal'} pt-[0px]`}>{props.name}</span>
-  </div>
+  </Link>
     )
 })
