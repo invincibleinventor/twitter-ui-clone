82e0fa72faac606f7f5b0e54c9d840308510857f
invincibleinventor <invincibleinventor@gmail.com>
2022-12-03 17:58:00 +0530

    ok

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,7 +20,7 @@ export default component$(() => {
  <div class="pt-5">
  <div class="flex flex-row rounded-full  md:w-[calc(220px)] mx-2 md:mr-auto  md:py-[14px] p-3 w-[52px] h-[52px] bg-blue-500 rounded-full transition-all ease-linear duration-100 cursor-pointer hover:bg-blue-600 items-center content-center">
     <span class={`text-white md:inline-block hidden text-[19px] text-center mx-auto  font-semibold pt-[0px]`}>Tweet</span>
-    <svg viewBox="0 0 24 24" aria-hidden="true" class="md:hidden w-[22px] h-[22px]   text-white mx-auto my-auto"><g><path d="M23 3c-6.62-.1-10.38 2.421-13.05 6.03C7.29 12.61 6 17.331 6 22h2c0-1.007.07-2.012.19-3H12c4.1 0 7.48-3.082 7.94-7.054C22.79 10.147 23.17 6.359 23 3zm-7 8h-1.5v2H16c.63-.016 1.2-.08 1.72-.188C16.95 15.24 14.68 17 12 17H8.55c.57-2.512 1.57-4.851 3-6.78 2.16-2.912 5.29-4.911 9.45-5.187C20.95 8.079 19.9 11 16 11zM4 9V6H1V4h3V1h2v3h3v2H6v3H4z"></path></g></svg>
+    <svg viewBox="0 0 24 24" aria-hidden="true" class="md:hidden w-[22px] h-[22px]  text-white mx-auto my-auto"><g><path d="M23 3c-6.62-.1-10.38 2.421-13.05 6.03C7.29 12.61 6 17.331 6 22h2c0-1.007.07-2.012.19-3H12c4.1 0 7.48-3.082 7.94-7.054C22.79 10.147 23.17 6.359 23 3zm-7 8h-1.5v2H16c.63-.016 1.2-.08 1.72-.188C16.95 15.24 14.68 17 12 17H8.55c.57-2.512 1.57-4.851 3-6.78 2.16-2.912 5.29-4.911 9.45-5.187C20.95 8.079 19.9 11 16 11zM4 9V6H1V4h3V1h2v3h3v2H6v3H4z"></path></g></svg>
   </div>
   </div>
 
